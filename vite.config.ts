aedab24308f5be6ebc0ea62d48f574c437acead8
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-09 10:13:16 +0900

    変更 - BaseURLミスを修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yumemi-codecheck-population-chart/',
+  base: '/codecheck-private-check/',
   build: {
     rollupOptions: {
       output: {
