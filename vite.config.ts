902ef4fbcb6f4ac2cda4eef94412ccfbb51e3008
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-09 10:08:12 +0900

    変更 -チャンクが多すぎる警告がでたので設定を変えてみる

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/codecheck-private-check/',
+  base: '/yumemi-codecheck-population-chart/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@/': `${__dirname}/`,
