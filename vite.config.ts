3be9ec1f7721e19b4a8dd43308894d4564cf6c94
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-09 10:18:34 +0900

    変更 - ビルド回りの設定を除外してみる

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/codecheck-private-check/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
   resolve: {
     alias: {
       '@/': `${__dirname}/`,
